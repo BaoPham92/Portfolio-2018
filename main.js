87d4f1c9de3b2a1de02e4bc1afaccbeef3ce239b
baopham92 <baopham92@yahoo.com>
2018-06-15 23:32:46 -0400

    Type: Adjustments. Body: Added comments to HTML section and rewrote javascript to clean up code.

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-window.onscroll = function () { scrollFunction() };
+// Grab navbar
+var nav = document.getElementById("navbar");
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
-        document.getElementById("navbar").style.top = "-50px";
-    }
+// Change position of bar on scroll
+window.onscroll = function () { navAppear() };
+
+function navAppear() {
+    document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ? nav.style.top = "0" : nav.style.top = "50px";
 }
